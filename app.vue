bdb4f7ee450fb4e35481a01b1ff540031a719849
Bas Buvelot <s.f.buvelot@gmail.com>
2023-12-18 16:31:47 +0100

    init test

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <QCard 
+      title="The Coldest Sunset"
+      description="lorem ipsum dolar"
+      :clicked="klik"
+    >
+    </QCard>
+  </div>
+</template>
+
+<script setup>
+const klik = () => {
+  console.log('klik')
+}
+</script>
+<style scoped>
+</style>
